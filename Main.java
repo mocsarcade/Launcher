5a5d04ee2014687cbbcc20e3f0451c566602f693
kingoftheconnors <Connormcp777@gmail.com>
2018-09-10 09:29:42 -0400

    Made first class

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+* Main class for GUI Launcher. This class opens all menus and calls the keybinders and GUI elements
+*
+* @author Connor McPherson
+* @version 1.0
+*/
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Working!");
+  }
+
+}
